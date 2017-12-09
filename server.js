57ac0359923fa6daf55545d65153c108d5f96452
Nelu Platonov <platon.nelu@yahoo.com>
2017-12-09 20:04:20 +0100

    Prettified Code using VSCode Prettier extension. Very useful tbh, will always use from now on.

--- server.js
+++ server.js
@@ -1,19 +1,17 @@
 const express = require("express"),
-      app = express(),
-      bodyParser = require("body-parser"),
-      ejs = require("ejs"),
-      path = require("path");
+  app = express(),
+  bodyParser = require("body-parser"),
+  ejs = require("ejs"),
+  path = require("path");
 
-app.use(bodyParser.urlencoded({extended: true}));
+app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "views/assets")));
 
-
-app.get("/", function(req, res){
-    res.render("index");
+app.get("/", function(req, res) {
+  res.render("index");
 });
 
-
-app.listen(process.env.PORT || 3000, function(error){
-    console.log("SERVER STARTED ON PORT 3000");
+app.listen(process.env.PORT || 3000, function(error) {
+  console.log("SERVER STARTED ON PORT 3000");
 });
