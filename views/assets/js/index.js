1d09370f7f74dcedd97f253789f82f0f084775b5
Nelu Platonov <platon.nelu@yahoo.com>
2017-12-10 16:21:47 +0100

    Added animations where I wanted and adjusted the lightbulb image height. Should do cross-browser test next, after that gonna ad a Mailer option for a news letter + email function for problems. After that time for a new page

--- views/assets/js/index.js
+++ views/assets/js/index.js
@@ -1,10 +1,14 @@
 var imgNumber = 1;
+var changedTextAmount = 0;
 $("#investmentsBtn").click(function() {
+  $("#handShakeImg").fadeOut(50);
+  $("#handShakeImg").fadeIn(200);
   imgNumber++;
   if (imgNumber === 4) {
     imgNumber = 1;
   };
   $("#handShakeImg").attr("src", "/images/handShake" + imgNumber + ".jpg");
+  
   for (var i = 1; i <= $("#investmentLastElem .fa.fa-circle").length; i++) {
     if (i === imgNumber) {
       var previousElem;
@@ -15,4 +19,12 @@ $("#investmentsBtn").click(function() {
       );
     }
   }
+});
+
+$("#interruptionPart button").click(function(){
+  changedTextAmount === 3 ? changedTextAmount = 3 : changedTextAmount++;
+  $("#interruptionLastElem i:nth-child("+ changedTextAmount +")").addClass("activeCircle");
+  $(this.nextSibling.nextSibling).fadeOut(0);
+  $(this.nextSibling.nextSibling).text("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris blandit aliquet elit, eget tincidunt nibh pulvinar a.")
+  $(this.nextSibling.nextSibling).fadeIn();
 });
