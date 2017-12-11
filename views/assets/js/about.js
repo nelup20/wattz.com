4db250af4f79a5af5f895b59876299cdccaf0cc4
Nelu Platonov <platon.nelu@yahoo.com>
2017-12-11 16:20:44 +0100

    Added about page text + pretty cool function to display the text with an interval

--- views/assets/js/about.js
+++ views/assets/js/about.js
@@ -1,8 +1,83 @@
-
-$(document).ready(function(){
-    $("#whoAreWe h2").css({
-        "padding-top": "150px",
-        "opacity": "1",
-        "transition": "all 1s"
-    });
+var title = $("#aboutWattz h3")
+  .text()
+  .split("");
+var charNumber = 0;
+var textCharNumber = 0;
+$(document).ready(function() {
+  $("#whoAreWe h2").css({
+    "padding-top": "150px",
+    opacity: "1",
+    transition: "all 1s"
+  });
+  setTimeout(function() {
+    var wholeTitle = "";
+    $("#aboutWattz i.fa.fa-spinner.fa-pulse.fa-3x.fa-fw").fadeOut(100);
+    $("#aboutWattz h3")
+      .text("")
+      .fadeIn(700);
+    var interval = setInterval(function() {
+      wholeTitle += title[charNumber];
+      $("#aboutWattz h3").text(wholeTitle);
+      if (charNumber < title.length - 1) {
+        charNumber++;
+      } else {
+        clearInterval(interval);
+        charNumber = 0;
+      }
+    }, 400);
+    (function() {
+      var wholeText = "";
+      var text = $("#aboutWattz :nth-child(3)")
+        .text()
+        .split("");
+      $("#aboutWattz :nth-child(3)")
+        .text("")
+        .fadeIn(700);
+      var textInterval1 = setInterval(function() {
+        wholeText += text[textCharNumber];
+        $("#aboutWattz :nth-child(3)").text(wholeText);
+        if (textCharNumber < text.length - 1) {
+          textCharNumber++;
+        } else {
+          clearInterval(textInterval1);
+          textCharNumber = 0;
+          wholeText = "";
+          text = $("#aboutWattz :nth-child(4)")
+            .text()
+            .split("");
+          $("#aboutWattz :nth-child(4)")
+            .text("")
+            .fadeIn();
+          var textInterval2 = setInterval(function() {
+            wholeText += text[textCharNumber];
+            $("#aboutWattz :nth-child(4)").text(wholeText);
+            if (textCharNumber < text.length - 1) {
+              textCharNumber++;
+            } else {
+              clearInterval(textInterval2);
+              textCharNumber = 0;
+              wholeText = "";
+              text = $("#aboutWattz :nth-child(5)")
+                .text()
+                .split("");
+              $("#aboutWattz :nth-child(5)")
+                .text("")
+                .fadeIn();
+              var textInterval3 = setInterval(function() {
+                wholeText += text[textCharNumber];
+                $("#aboutWattz :nth-child(5)").text(wholeText);
+                if (textCharNumber < text.length - 1) {
+                  textCharNumber++;
+                } else {
+                  clearInterval(textInterval3);
+                  textCharNumber = 0;
+                  wholeText = "";
+                }
+              }, 40);
+            }
+          }, 40);
+        }
+      }, 40);
+    })();
+  }, 3500);
 });
