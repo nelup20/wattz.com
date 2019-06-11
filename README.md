3dc56a6b276dcd54069bf6b2427a563a48b56547
Nelu Platonov <platon.nelu@yahoo.com>
2019-06-11 14:17:21 +0200

    Updated README

--- README.md
+++ README.md
@@ -1,1 +1,20 @@
-# wattz.com
+# This code is absolutely awful and I'll explain why and what could be better
+
+I created my main 3 personal projects (The personal website, fake/mock restaurant site and the fake/mock electricity company site) roughly 7-9 months after I started learning programming. As I'm updating this Readme it's been 1.5 years and the code you will see doesn't represent my current level of skill and knowledge.
+
+I haven't managed to do any serious refactoring or improvements due to University and for now can only mention what was done badly and what can be improved as well as potentially what other technologies I could've used to improve the projects.
+
+# Things that can be improved
+- [ ] Not enough documentation, lack of comments. In a professional/team setting this is a must.
+- [ ] 0 testing was done, neither unit or integration testing. At that point I hadn't learned testing but now I would use Jest + Puppeteer
+- [ ] CSS is abysmal, no variable usage, way too many repeating/overlapping rules, sizes are bad/incosistent (didn't know about using REM while setting root font size to ex: 62.5%), no splitting of styles into components/modules (ie no architecture, didn't know about BEM, didn't know Sass)
+- [ ] Nothing done in terms of accessibility
+- [ ] Nothing done in terms of security (should've at least used Helmet.js, didn't even know about OWASP top 10)
+- [ ] Deployed against Node only, no proper clustering & restart management (should've used PM2)
+- [ ] Back-end/Server is just 1 big mess of a file, should've split & modularised it, didn't use Express' router, architecture is just bad and not scalable
+- [ ] No usage of CDNs to serve static assets such as images
+- [ ] No continuous integration set up such as with Travis CI, would just manually deploy to heroku
+- [ ] Commits were always done on Master branch, to simulate a professional working environment I should've commited on separate branches
+- [ ] Should have created the Front-End's Design/CSS from actual Photoshop/Sketch files or any other design tool (to know exact measurements/sizes in px) instead of using plain images/JPEGs and eyeballing the sizes and proportions
+
+And there may be potentially many more things that could be improved (perhaps using Docker but I feel like that would be overkill for these small projects), that's one of the fun parts of Software Development, there's always more new stuff to learn. 
--- views/assets/js/ticketStatus.js
+++ views/assets/js/ticketStatus.js
@@ -1,33 +1,38 @@
+let notFound = 0;
 
-    let notFound = 0;
-
-    $(document).ready(function(){
-        $(".btn.btn-success").click(function(e){
-            e.preventDefault();
-            $.ajax({
-                url: "/ticket/status",
-                type: "POST",
-                data: {
-                    ticketNumber: $(".form-control").val()
-                },
-                success: function(data ,textstatus, jqXHR){
-                    console.log(data)
-                    if(data === "None found"){
-                            if(notFound === 0){
-                                $("#ticketStatusContainer button").css("margin-bottom", "50px");
-                                $("#ticketStatusContainer").append('<h3 class="btn btn-danger form-control col-xs-12" id="wrongNumberWarning">Ticket not found! Please make sure you put in the correct ticket number.</h3>');
-                                notFound = 1;
-                            }
-                    } else {
-                        $("#ticketStatusContainer").html("<i class='fa fa-cog fa-spin fa-3x fa-fw' id='loadingIcon'></i>");
-                        $("#ticketStatusContainer").css("text-align", "center");
-                        setTimeout(function(){
-                            $("#ticketStatusContainer").css("text-align", "left");
-                            let status;
-                            data[0].solved === false ? status = "In progress" : status = "Closed";
-                            $("#ticketStatusContainer").fadeOut(0);
-                            $("#ticketStatusContainer").fadeIn();
-                            $("#ticketStatusContainer").html(`
+$(document).ready(function() {
+  $(".btn.btn-success").click(function(e) {
+    e.preventDefault();
+    $.ajax({
+      url: "/ticket/status",
+      type: "POST",
+      data: {
+        ticketNumber: $(".form-control").val()
+      },
+      success: function(data, textstatus, jqXHR) {
+        console.log(data);
+        if (data === "None found") {
+          if (notFound === 0) {
+            $("#ticketStatusContainer button").css("margin-bottom", "50px");
+            $("#ticketStatusContainer").append(
+              '<h3 class="btn btn-danger form-control col-xs-12" id="wrongNumberWarning">Ticket not found! Please make sure you put in the correct ticket number.</h3>'
+            );
+            notFound = 1;
+          }
+        } else {
+          $("#ticketStatusContainer").html(
+            "<i class='fa fa-cog fa-spin fa-3x fa-fw' id='loadingIcon'></i>"
+          );
+          $("#ticketStatusContainer").css("text-align", "center");
+          setTimeout(function() {
+            $("#ticketStatusContainer").css("text-align", "left");
+            let status;
+            data[0].solved === false
+              ? (status = "In progress")
+              : (status = "Closed");
+            $("#ticketStatusContainer").fadeOut(0);
+            $("#ticketStatusContainer").fadeIn();
+            $("#ticketStatusContainer").html(`
                             <div class="container" id="ticketStatusContainer">
                             <h1>Ticket Status</h1>
                             <h3>Ticket number: ${data[0].id}</h3>
@@ -36,9 +41,9 @@
                             <h3 id='ticketStatus'>Status: ${status}</h3>
                         </div>
                         `);
-                        }, 2000);
-                    }
-                }
-            });
-        });
+          }, 2000);
+        }
+      }
     });
+  });
+});
